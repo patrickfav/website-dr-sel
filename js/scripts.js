84d30706fe67db5c647dc72f5c1f1fe610a17746
Patrick Favre-Bulle <patrick.favrebulle@gmail.com>
2018-06-17 21:55:00 +0200

    Inline more js and css

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+---
+# to let it process by the template engine
+---
+
+{% include js/jquery-2.2.4.min.js %}
+{% include js/jquery-scrollex-0.2.1.min.js %}
+{% include js/jquery-scrolly-1.0.0.min.js %}
+{% include js/skel-3.0.1.min.js %}
+{% include js/custom_script.js %}
